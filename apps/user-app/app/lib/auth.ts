27184149f000ff4e882157fab1024fdb3c5c3095
Ashraf <ashraf@dell-inspiron.lan>
2024-05-27 12:00:40 -0500

    Updated db schema

---
+++ apps/user-app/app/lib/auth.ts
@@ -0,0 +1,25 @@
+import { PrismaClient } from "@repo/db/client";
+import { CredentialsProvider } from "next-auth/providers/credentials";
+import bcrypt from 'bcrypt';
+
+const db = new PrismaClient();
+
+export const authOptions = {
+    providers: [
+        CredentialsProvider({
+            name: 'Credentials',
+            credentials: {
+                phone: { label: "Phone Number", type: "text", placeholder: "0000000000" },
+                password: { label: "Password", type: "password" }
+            },
+            async authorize(credentials: any) {
+                const hashedPassword = await bcrypt.hash(credentials.password, 10);
+                const existingUser = await db.user.findFirst({
+                    where: {
+                        number: credentials.phone
+                    }
+                })
+            }
+        })
+    ]
+}
