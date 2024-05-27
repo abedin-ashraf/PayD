b3252b4e2fccc81d5dc5e94cf10efce4feee092f
Ashraf <ashraf@dell-inspiron.lan>
2024-05-27 11:42:58 -0500

    Updated Schema in DB

--- apps/merchant-app/app/page.tsx
+++ apps/merchant-app/app/page.tsx
@@ -1,13 +1,15 @@
+"use client"
 import Image from "next/image";
 import { Card } from "@repo/ui/card";
 import { Code } from "@repo/ui/code";
 import styles from "./page.module.css";
 import { Button } from "@repo/ui/button";
-
+import { useBalance } from "@repo/store/useBalance"
 
 
 export default function Page(): JSX.Element {
+  const balance = useBalance();
   return (
-    <div>Merchant App</div>
+    <div>Merchant App {balance}</div>
   );
 }
