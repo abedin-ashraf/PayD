4767700e5b4600e4b86d0471a1581511e2129714
Ashraf <ashraf@dell-inspiron.lan>
2024-05-27 12:18:40 -0500

    Added authentication

--- apps/user-app/app/lib/auth.ts
+++ apps/user-app/app/lib/auth.ts
@@ -1,5 +1,5 @@
 import { PrismaClient } from "@repo/db/client";
-import { CredentialsProvider } from "next-auth/providers/credentials";
+import CredentialsProvider from "next-auth/providers/credentials";
 import bcrypt from 'bcrypt';
 
 const db = new PrismaClient();
@@ -18,8 +18,46 @@ export const authOptions = {
                     where: {
                         number: credentials.phone
                     }
-                })
+                });
+
+                if (existingUser) {
+                    const passwordValidation = await bcrypt.compare(credentials.password, existingUser.password);
+                    if (passwordValidation) {
+                        return {
+                            id: existingUser.id.toString(),
+                            name: existingUser.name,
+                            email: existingUser.email
+                        }
+                    }
+                    return null;
+                }
+
+                try {
+                    const user = await db.user.create({
+                        data: {
+                            number: credentials.phone,
+                            password: hashedPassword
+                        }
+                    });
+                    return {
+                        id: user.id.toString(),
+                        name: user.name,
+                        number: user.number,
+                    }
+                }
+                catch (err) {
+                    console.error(err);
+                }
+                return null;
             }
         })
-    ]
+    ],
+    secret: process.env.JWT_SECRET || 'secret',
+    callbacks: {
+        async session({ token, session }: any) {
+            session.user.id = token.sub;
+
+            return session;
+        }
+    }
 }
